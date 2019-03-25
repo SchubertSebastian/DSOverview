16bc7264ff4b76020889451c6d7511ffd3fecafd
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-03-25 13:37:19 +0100

    - js test

---
+++ src/script/jscript.js
@@ -0,0 +1,14 @@
+
+
+function getAjax(url, success) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
+}
+
+getAjax('http://leotux.htl-leonding.ac.at/display', function(data){ console.log(data); });

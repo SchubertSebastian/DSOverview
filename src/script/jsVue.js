d855477dfd3595a49514836acde09cf51bcd64c8
Sebastian Schubert <39259189+C0rruptdeX@users.noreply.github.com>
2019-05-21 18:08:52 +0200

    - add test Vue.component

--- src/script/jsVue.js
+++ src/script/jsVue.js
@@ -1,6 +1,15 @@
-new Vue({
-  el: '#root',
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+  el: '#app',
   data: {
-    greeting: 'fuck you'
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
